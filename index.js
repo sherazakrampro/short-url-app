af8e37f74eea57b6ab09ee70b1bde1f52db518c9
Sheraz Akram <sherazakram.pro@gmail.com>
2024-05-22 20:45:12 +0500

    url route

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 require("dotenv").config();
 const connectDB = require("./db/connectDB");
+const urlRoute = require("./routes/url");
 
 connectDB(process.env.MONGO_URL);
 
 const app = express();
 app.use(express.json());
+
+app.use("/url", urlRoute);
 
 const PORT = process.env.PORT || 5000;
 
