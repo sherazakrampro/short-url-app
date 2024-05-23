b41109beebd3e1bf41d031de96f593708b91b3cd
Sheraz Akram <sherazakram.pro@gmail.com>
2024-05-23 15:58:09 +0500

    auth middleware

---
+++ middlewares/auth.js
@@ -0,0 +1,13 @@
+const { getUser } = require("../service/auth");
+
+const restrictToLoggedInUser = async (req, res, next) => {
+  const userUid = req.cookies.uid;
+  if (!userUid) return res.redirect("/login");
+
+  const user = await getUser(userUid);
+  if (!user) return res.redirect("/login");
+  req.user = user;
+  next();
+};
+
+module.exports = { restrictToLoggedInUser };
